4957dc1420ec7e2d93f5a83375808f82a8968359
Craig Chambers <craigchambers@craigs-mac-mini.home>
2019-10-30 15:14:05 +0000

    Bug fixes on border padding for mobile and desktop

---
+++ resources/js/index.js
@@ -0,0 +1,14 @@
+/*
+Card HTML for reference
+
+<li class="card">
+    <div class="discipline">
+        <div class="image"></div>
+        <h1 class="disciplineheader">Empathy</h1>
+        <h4 class="disciplinetagline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+        sint!
+        </h4>
+    </div>
+</li>
+*/
