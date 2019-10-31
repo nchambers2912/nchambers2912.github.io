a1fbe090a6a9781cbfef197366f71c13d3806f8f
Craig Chambers <craigchambers@craigs-mac-mini.home>
2019-10-31 08:57:00 +0000

    Added draft Javascript

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,62 @@
-/*
-Card HTML for reference
+var cards = document.querySelector(".cards");
 
-<li class="card">
-    <div class="discipline">
-        <div class="image"></div>
-        <h1 class="disciplineheader">Empathy</h1>
-        <h4 class="disciplinetagline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-        sint!
-        </h4>
-    </div>
-</li>
-*/
+window.addEventListener("load", function() {
+  var data = getData();
+
+  //   data.forEach(element => {
+  //     var card = buildCard(element.title, element.description, element.image);
+  //     this.console.log(card);
+  //     cards.appendChild(card);
+  //   });
+});
+
+function getData() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "../../resources/json/types.json", false);
+  request.send();
+  var data = JSON.parse(request.responseText);
+  return data;
+}
+
+function buildCard(title, tagline, image) {
+  /*
+    Card HTML for reference
+
+    <li class="card">
+        <div class="discipline">
+            <div class="image"></div>
+            <h1 class="disciplineheader">Empathy</h1>
+            <h4 class="disciplinetagline">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+            sint!
+            </h4>
+        </div>
+    </li>
+    */
+
+  var card = document.createElement("li");
+  card.classList.add("card");
+
+  var discipline = document.createElement("div");
+  discipline.classList.add("discipline");
+  card.appendChild(discipline);
+
+  var div = document.createElement("div");
+  div.classList.add("image");
+  var img = document.createElement("img");
+  img.setAttribute("src", image);
+  div.appendChild(img);
+  discipline.appendChild(div);
+
+  var theTitle = document.createElement("h1");
+  theTitle.classList.add("disciplineheader");
+  theTitle.innerHTML = title;
+  discipline.appendChild(theTitle);
+
+  var theTagline = document.createElement("h4");
+  theTagline.classList.add("diciplinetagline");
+  theTagline.innerHTML = tagline;
+  discipline.appendChild(theTagline);
+
+  return card;
+}
